cf666c536806043fa5c2912c4ff33238928e856c
Tinius <tiniusw@uio.no>
2025-07-29 15:56:27 +0200

    Basic neural

--- Code/main.py
+++ Code/main.py
@@ -1,30 +1,32 @@
-import cv2 #For computer vision tasks
-import numpy as np #For numerical operations and arrays
-import matplotlib.pyplot as plt #For plotting and visualizing data
-import tensorflow as tf #For building and training neural networks
-import os 
+import numpy as np
+import os
+from keras.datasets import mnist
 
-#Importing the MNIST dataset and splitting into training and testing data
-mnist = tf.keras.datasets.mnist 
-trainingData = mnist.load_data()[0]
-testData = mnist.load_data()[1] 
+mnistDataSet = mnist.load_data()
 
-(x_train,y_train),(x_test, y_test) = trainingData, testData
+# Load the MNIST dataset from mnistDataSet variable
+def load_mnist_images():
+    (x_train, _), (x_test, _) = mnistDataSet
+    x_train = x_train.reshape(-1, 28*28) / 255.0
+    x_test = x_test.reshape(-1, 28*28) / 255.0
+    return x_train, x_test
 
+def load_mnist_labels():
+    (_, y_train), (_, y_test) = mnistDataSet
+    return y_train, y_test
 
-x_train = tf.keras.utils.normalize(x_train, axis=1) 
-x_test = tf.keras.utils.normalize(x_test, axis=1)
+def init_params(input_size, hidden_size, output_size):
+    W1 = np.random.randn(input_size, hidden_size) * 0.01 
+    b1 = np.zeros((1, hidden_size)) 
+    W2 = np.random.randn(hidden_size, output_size) * 0.01  
+    b2 = np.zeros((1, output_size))
+    return W1, b1, W2, b2
 
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(256, activation='relu')) #Utilizing rectified Linear Unit (RELU)
-model.add(tf.keras.layers.Dense(256, activation='relu'))
-model.add(tf.keras.layers.Dense(10, activation='softmax')) #OutPut Layer
+# Forward pass
+def relu(x): 
+    return np.maximum(0, x)
 
-#model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy']) 
-#model.fit(x_train, y_train, epochs=4)
-#model.save('number_recognition.keras')
+def softmax(x):
+    exp_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+    return exp_x / np.sum(exp_x, axis=1, keepdims=True)
 
-model = tf.keras.models.load_model('number_recognition.keras')
-loss, accuracy = model.evaluate(x_test, y_test)
-print(f"Loss: {loss}\n Accuracy: {accuracy}")
